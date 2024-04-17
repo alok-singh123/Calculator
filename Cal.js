6a40b45c51834c2019fb2b641372eb66833908f3
aloksingh <singhalok8968.in>
2024-04-17 16:19:21 +0530

    Add Calculator

---
+++ Cal.js
@@ -0,0 +1,27 @@
+let display=document.querySelector("#input");
+let btns=document.querySelectorAll("button");
+
+let buttonsArray=Array.from(btns);
+let string='';
+buttonsArray.forEach((btn)=>{
+    btn.addEventListener("click",(e)=>{
+        if(e.target.innerHTML=="DEL"){
+            string=string.substring(0,string.length-1);
+            display.value=string;
+        }else if(e.target.innerHTML=="AC"){
+           string='';
+           display.value=string;
+        }else if(e.target.innerHTML=="="){
+          string=eval(string);
+          display.value=string
+        }
+        else{
+        string+=e.target.innerHTML;
+        display.value=string;
+        }
+       
+    })
+   
+})
+
+
